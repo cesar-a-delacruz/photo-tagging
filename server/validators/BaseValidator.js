27a1e69985f651a7d068b7d043f5e4a6fdcc6623
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-02-06 01:22:33 -0500

    feat(server): add BaseValidator class
    This class accepts a schema object of express-validator and can
    add additional rules to fields if they are provided in another schema
    as a property withn a new object.

---
+++ server/validators/BaseValidator.js
@@ -0,0 +1,21 @@
+import { checkSchema } from "express-validator";
+export default class BaseValidator {
+  schemas = {};
+  constructor(
+    baseSchema,
+    validations = { create: undefined, update: undefined },
+  ) {
+    for (const validation in validations) {
+      this.schemas[validation] = baseSchema;
+      if (validations[validation] !== undefined) {
+        for (const field in validations[validation]) {
+          for (const rule in validations[validation][field]) {
+            this.schemas[validation][field][rule] =
+              validations[validation][field][rule];
+          }
+        }
+      }
+      this[validation] = checkSchema(this.schemas[validation]);
+    }
+  }
+}

45aaef1eb295aa5f9cfa2d5ad465d1ace73fbe75
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-02-12 23:11:56 -0500

    feat(server): add ImageController
    It extends RESTController by adding a new implementation of the findOne
    method.

---
+++ server/controllers/ImageController.js
@@ -0,0 +1,17 @@
+import RESTController from "./RESTController.js";
+
+export default class ImageController extends RESTController {
+  findOne = async (req, res) => {
+    try {
+      const row = await this.model.findUnique({
+        where: { id: Number(req.params.id) },
+        include: { objects: true },
+      });
+      console.info(row);
+      res.status(200).json(row);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to fetch item" });
+    }
+  };
+}
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,12 @@
 import RESTController from "./RESTController.js";
+import ImageController from "./ImageController.js";
 import { PrismaClient } from "../generated/prisma/index.js";
 import validators from "../validators/index.js";
 
 const prisma = new PrismaClient();
 
 export default {
-  image: new RESTController(
+  image: new ImageController(
     prisma.image,
     {
       name: "string",

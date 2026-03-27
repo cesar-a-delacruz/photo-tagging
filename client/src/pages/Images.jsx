f5d26aa4662de5b569229ce508202cb12d0b05b3
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-03-26 21:31:25 -0500

    refactor(client): Images to DataForm changes

--- client/src/pages/Images.jsx
+++ client/src/pages/Images.jsx
@@ -5,64 +5,71 @@ import useGetData from "@/hooks/useGetData";
 import requestHandler from "@/utils/requestHandler";
 import { useRef, useState } from "react";
 import { useOutletContext } from "react-router-dom";
+import { formDataReducer, formDataReseter } from "@/utils/objectHandler";
 
 export default function Images() {
   const setTitle = useOutletContext();
   setTitle("Images");
-
   const [images, setImages] = useGetData("image");
-  const [selectedImage, setSelectedImage] = useState({});
+
   const addDialog = useRef(null);
   const updateDialog = useRef(null);
   const deleteDialog = useRef(null);
 
-  const dataFields = [
-    { name: "id", value: selectedImage.id, type: "hidden" },
-    { name: "name", label: "Name", value: selectedImage.name, type: "text" },
-    { name: "url", label: "URL", value: selectedImage.url, type: "text" },
-  ];
+  const [formData, setFormData] = useState([
+    { name: "id", value: "", type: "hidden" },
+    { name: "name", label: "Name", value: "", type: "text" },
+    { name: "url", label: "URL", value: "", type: "text" },
+  ]);
 
   return (
     <>
       <div className="dialogs">
         <Dialog title={"Add Image"} ref={addDialog}>
           <DataForm
-            fields={dataFields.filter((field) => field.name !== "id")}
-            empty={true}
+            data={formData.filter((field) => field.name !== "id")}
+            setData={setFormData}
             action={{
               name: "Add",
               handler: async (data) => {
-                await requestHandler.post(data, "image");
+                data = formDataReducer(data);
+                data.id = undefined;
                 setImages([...images, data]);
+                await requestHandler.post(data, "image");
                 addDialog.current.close();
+                setFormData((prev) => formDataReseter(prev));
               },
             }}
           />
         </Dialog>
         <Dialog title={"Update Image"} ref={updateDialog}>
           <DataForm
-            fields={dataFields}
+            data={formData}
+            setData={setFormData}
             action={{
               name: "Update",
               handler: async (data) => {
+                data = formDataReducer(data);
                 await requestHandler.put(data, "image");
                 setImages(
                   images.map((image) => (image.id === data.id ? data : image)),
                 );
                 updateDialog.current.close();
+                setFormData((prev) => formDataReseter(prev));
               },
             }}
           />
         </Dialog>
         <Dialog title={"Delete Image"} ref={deleteDialog}>
           <AlertForm
-            field={dataFields.find((field) => field.name === "id")}
+            data={formData.find((field) => field.name === "id")}
             action={{
               name: "Delete",
               handler: async (data) => {
-                await requestHandler.delete(data.id, "image");
-                setImages(images.filter((image) => image.id !== data.id));
+                await requestHandler.delete(data.value, "image");
+                setImages(images.filter((image) => image.id !== data.value));
                 deleteDialog.current.close();
+                setFormData((prev) => formDataReseter(prev));
               },
             }}
           >
@@ -71,7 +78,7 @@ export default function Images() {
         </Dialog>
       </div>
 
-      <button onClick={() => (addDialog.current.open = true)}>Add</button>
+      <button onClick={() => addDialog.current.show()}>Add</button>
       <div className="images">
         {images &&
           images.map((image) => (
@@ -82,16 +89,31 @@ export default function Images() {
                   <a href={`images/${image.id}/objects`}>View Objects</a>
                   <button
                     onClick={() => {
-                      setSelectedImage(image);
-                      updateDialog.current.open = true;
+                      setFormData((prev) => {
+                        for (let i = 0; i < prev.length; i++) {
+                          if (
+                            prev[i].type === "json" &&
+                            typeof prev[i].value === "string"
+                          )
+                            prev[i].value = JSON.parse(image[prev[i].name]);
+                          else prev[i].value = image[prev[i].name];
+                        }
+                        return [...prev];
+                      });
+                      updateDialog.current.show();
                     }}
                   >
                     Update
                   </button>
                   <button
                     onClick={() => {
-                      setSelectedImage(image);
-                      deleteDialog.current.open = true;
+                      setFormData((prev) => {
+                        for (let i = 0; i < prev.length; i++) {
+                          prev[i].value = image[prev[i].name];
+                        }
+                        return [...prev];
+                      });
+                      deleteDialog.current.show();
                     }}
                   >
                     Delete

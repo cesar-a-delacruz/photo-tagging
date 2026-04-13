32cdfff8d988a7a1b8da66ccf2eb2131746c7980
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-04-12 20:37:55 -0500

    feat(ui): style Default layout and move CSS

--- client/src/layouts/Default.jsx
+++ client/src/layouts/Default.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
-import "@/styles/layouts/Default.css";
+import "./styles/Default.css";
 
 export default function Default() {
   return (
     <>
       <header>
-        <h1 onClick={() => location.assign("/")}>Photo Tagging</h1>
+        <h1 onClick={() => location.assign("/")}>
+          Photo Tagging <img src="/icons/tag.svg" alt="" />
+        </h1>
       </header>
       <main>
         <Outlet />

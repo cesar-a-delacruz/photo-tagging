d4c5d8b168bb08b501b903c6acf1a6b2b9483c70
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-04-14 14:41:41 -0500

    fix(ui): set Default CSS module and common CSS for layouts

--- client/src/layouts/Default.jsx
+++ client/src/layouts/Default.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
-import "./styles/Default.css";
+import "./styles/layouts.css";
+import styles from "./styles/Default.module.css";
 
 export default function Default() {
   return (
-    <div className="layout default">
+    <div className={`layout ${styles.default}`}>
       <header>
         <h1 onClick={() => location.assign("/")}>
           Photo Tagging <img src="/icons/tag.svg" alt="" />

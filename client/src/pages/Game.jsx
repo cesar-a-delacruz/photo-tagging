b8ffa25b1581b85492dee95a4cace521eb40f5cb
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-04-26 09:59:48 -0500

    feat(ui): style Game page when playing and ad icons

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -17,7 +17,7 @@ export default function Game() {
   document.title = `${import.meta.env.VITE_TITLE}: Game`;
 
   const [images] = useData("image");
-  const [image, setImage] = useState(null);
+  const [gameImage, setGameImage] = useState(null);
   const [score, setScore] = useState({ record: "" });
   const [pinPosition, setPinPosition] = useState(null);
   const [game, setGame] = useState({
@@ -34,7 +34,6 @@ export default function Game() {
     { name: "name", label: "Name", value: "", type: "text" },
   ]);
 
-  const imageDialog = useRef(null);
   const recordDialog = useRef(null);
   const winDialog = useRef(null);
   const deleteDialog = useRef(null);
@@ -54,48 +53,6 @@ export default function Game() {
     <GameContext value={game}>
       <div className="page game">
         <div className="dialogs">
-          <Dialog title={"Select Image"} ref={imageDialog}>
-            <a href="/images">All Images</a>
-            <div className="images">
-              {images &&
-                images.map((image) => (
-                  <div className="image" key={image.id}>
-                    <img src={image.url} alt="" />
-                    <h4>{image.name}</h4>
-                    <button
-                      onClick={async () => {
-                        const imageResult = await requestHandler.get(
-                          `image/${image.id}`,
-                        );
-                        if (!imageResult.objects.length) {
-                          alert("The image doesn't have objects");
-                          return;
-                        }
-                        let scoreVal = { record: "00:00" };
-
-                        if (userId) {
-                          const scoreResult = await requestHandler.get(
-                            `score/user/${userId}/image/${image.id}`,
-                          );
-                          if (scoreResult.data) scoreVal = scoreResult.data;
-                          console.log(scoreResult.data);
-                        }
-                        setScore(scoreVal);
-                        setImage(imageResult);
-                        setGame((prev) => ({
-                          ...prev,
-                          start: true,
-                          stop: false,
-                        }));
-                        imageDialog.current.close();
-                      }}
-                    >
-                      Select
-                    </button>
-                  </div>
-                ))}
-            </div>
-          </Dialog>
           <Dialog title={"Reset Record"} ref={recordDialog}>
             <AlertForm
               data={{ name: "id", value: score.id, type: "hidden" }}
@@ -138,7 +95,7 @@ export default function Game() {
                   dispatchFormData({ type: "clear" });
 
                   setScore({ record: "" });
-                  setImage(null);
+                  setGameImage(null);
                   setGame((prev) => {
                     prev.stop = false;
                     prev.start = false;
@@ -173,7 +130,7 @@ export default function Game() {
                   const scoreData = {
                     record: score.record,
                     userId: id,
-                    imageId: image.id,
+                    imageId: gameImage.id,
                   };
                   const scoreResult = await requestHandler.post(
                     scoreData,
@@ -186,83 +143,84 @@ export default function Game() {
             />
           </Dialog>
         </div>
-        <div className="top">
-          <div className="data">
-            <div className="user">
-              {userId && (
-                <p>
-                  Name: <span>{formData[1].value}</span>
-                </p>
-              )}
+        <div className={styles.top}>
+          <h2>{gameImage ? gameImage.name : ""} Game</h2>
+          {userId && (
+            <div className={styles.data}>
+              <div className={styles.item}>
+                <img src="/icons/user.svg" alt="User name" className="icon" />
+                <span>Name:</span> {formData[1].value}
+              </div>
               {score.record !== "" && score.record !== "00:00" && (
-                <p>
-                  Record: <span>{score.record}</span>
-                </p>
+                <div className={styles.item}>
+                  <img
+                    src="/icons/timer.svg"
+                    alt="User record"
+                    className="icon"
+                  />
+                  <span>Record:</span> {score.record}
+                </div>
               )}
             </div>
-            {(score.record === "00:00" || game.start) && (
-              <Timer
-                setRecord={async (timeString) => {
-                  setScore((prev) => ({ ...prev, record: timeString }));
-
-                  if (!userId) {
-                    winDialog.current.show();
-                    return;
-                  }
-
-                  const scoreData = {
-                    record: timeString,
-                    userId: parseInt(userId),
-                    imageId: image.id,
-                  };
-                  await requestHandler.post(scoreData, "score");
-                }}
-                stop={game.stop}
-                start={game.start}
-                record={score.record}
-              />
-            )}
-          </div>
-          {image && (
+          )}
+          {gameImage && (
             <div className="options">
               <h3>Options:</h3>
-              {image && (
+              {gameImage && (
                 <button
+                  className="option"
                   onClick={() => {
                     setGame((prev) => ({ ...prev, stop: true, start: false }));
-                    imageDialog.current.show();
+                    setGameImage(null);
+                    setScore({ record: "" });
                   }}
                 >
-                  Select Image
+                  <img
+                    src="/icons/images.svg"
+                    alt="Select another image"
+                    className="icon"
+                  />
+                  Select another image
                 </button>
               )}
               {score.record !== "" && score.record !== "00:00" && userId && (
                 <button
+                  className="option"
                   onClick={() => {
-                    if (!score || !score.id) return;
-                    recordDialog.current.show();
+                    if (!score.id) return;
+                    recordDialog.current.showModal();
                   }}
                 >
-                  Reset Record
+                  <img
+                    src="/icons/timer-reset.svg"
+                    alt="Reset Record"
+                    className="icon"
+                  />
+                  Reset record
                 </button>
               )}
               {userId && (
                 <button
+                  className="option"
                   onClick={() => {
                     if (!userId) return;
-                    deleteDialog.current.show();
+                    deleteDialog.current.showModal();
                   }}
                 >
-                  Delete Data
+                  <img
+                    src="/icons/user-delete.svg"
+                    alt="Delete Data"
+                    className="icon"
+                  />
+                  Delete data
                 </button>
               )}
             </div>
           )}
         </div>
         <div className={styles.bottom}>
-          {images && !image && (
+          {images && !gameImage && (
             <>
-              <h2>Game</h2>
               <h3>
                 Select an Image or <a href="/images">edit them</a>
               </h3>
@@ -287,10 +245,9 @@ export default function Game() {
                             `score/user/${userId}/image/${image.id}`,
                           );
                           if (scoreResult.data) scoreVal = scoreResult.data;
-                          console.log(scoreResult.data);
                         }
                         setScore(scoreVal);
-                        setImage(imageResult);
+                        setGameImage(imageResult);
                         setGame((prev) => ({
                           ...prev,
                           start: true,
@@ -304,66 +261,103 @@ export default function Game() {
               </div>
             </>
           )}
-          {image && (
-            <>
-              <h3>{image.name}</h3>
-              <img
-                src={image.url}
-                alt={image.name}
-                onClick={(e) => {
-                  if (game.stop || !game.start) return;
+          {gameImage && (
+            <div className={styles.gameContainer}>
+              <div className={styles.left}>
+                <img
+                  src={gameImage.url}
+                  alt={gameImage.name}
+                  onClick={(e) => {
+                    if (game.stop || !game.start) return;
 
-                  const imgBoundingSides = {
-                    left: e.currentTarget.getBoundingClientRect().left,
-                    top: e.currentTarget.getBoundingClientRect().top,
-                  };
-                  const imgSizeRatio = {
-                    width:
-                      e.currentTarget.naturalWidth /
-                      e.currentTarget.clientWidth,
-                    height:
-                      e.currentTarget.naturalHeight /
-                      e.currentTarget.clientHeight,
-                  };
-                  const clickedPosition = {
-                    x: (e.clientX - imgBoundingSides.left) * imgSizeRatio.width,
-                    y: (e.clientY - imgBoundingSides.top) * imgSizeRatio.height,
-                  };
-                  const pinCenter = 37;
-                  image.objects.forEach((object) => {
-                    if (
-                      clickedPosition.x < object.position.x + pinCenter &&
-                      clickedPosition.x > object.position.x - pinCenter &&
-                      clickedPosition.y < object.position.y + pinCenter &&
-                      clickedPosition.y > object.position.y - pinCenter
-                    ) {
-                      setGame((prev) => ({
-                        ...prev,
-                        objects: { current: object, found: prev.objects.found },
-                      }));
-                    }
-                  });
-                  setPinPosition({ x: e.pageX, y: e.pageY });
-                }}
-              />
-            </>
-          )}
-          {image && (
-            <div className="objects">
-              <h3>Objects to find</h3>
-              <ol>
-                {image.objects.map((object) => (
-                  <li key={object.id}>{object.name}</li>
-                ))}
-              </ol>
+                    const imgBoundingSides = {
+                      left: e.currentTarget.getBoundingClientRect().left,
+                      top: e.currentTarget.getBoundingClientRect().top,
+                    };
+                    const imgSizeRatio = {
+                      width:
+                        e.currentTarget.naturalWidth /
+                        e.currentTarget.clientWidth,
+                      height:
+                        e.currentTarget.naturalHeight /
+                        e.currentTarget.clientHeight,
+                    };
+                    const clickedPosition = {
+                      x:
+                        (e.clientX - imgBoundingSides.left) *
+                        imgSizeRatio.width,
+                      y:
+                        (e.clientY - imgBoundingSides.top) *
+                        imgSizeRatio.height,
+                    };
+                    const pinCenter = 37;
+                    gameImage.objects.forEach((object) => {
+                      if (
+                        clickedPosition.x < object.position.x + pinCenter &&
+                        clickedPosition.x > object.position.x - pinCenter &&
+                        clickedPosition.y < object.position.y + pinCenter &&
+                        clickedPosition.y > object.position.y - pinCenter
+                      ) {
+                        setGame((prev) => ({
+                          ...prev,
+                          objects: {
+                            current: object,
+                            found: prev.objects.found,
+                          },
+                        }));
+                      }
+                    });
+                    setPinPosition({ x: e.pageX, y: e.pageY });
+                  }}
+                />
+              </div>
+              <div className={styles.right}>
+                <div className={styles.rightContainer}>
+                  {(score.record === "00:00" || game.start) && (
+                    <Timer
+                      setRecord={async (timeString) => {
+                        setScore((prev) => ({ ...prev, record: timeString }));
+
+                        if (!userId) {
+                          winDialog.current.showModal();
+                          return;
+                        }
+
+                        const scoreData = {
+                          record: timeString,
+                          userId: userId,
+                          imageId: gameImage.id,
+                        };
+                        const result = await requestHandler.post(
+                          scoreData,
+                          "score",
+                        );
+                        if (result.data)
+                          setScore((prev) => ({ ...prev, id: result.data.id }));
+                      }}
+                      stop={game.stop}
+                      start={game.start}
+                      record={score.record}
+                    />
+                  )}
+                  <div className={styles.objects}>
+                    <h3>Objects to find</h3>
+                    <ol>
+                      {gameImage.objects.map((object) => (
+                        <li key={object.id}>{object.name}</li>
+                      ))}
+                    </ol>
+                  </div>
+                </div>
+              </div>
             </div>
           )}
-          {pinPosition && image && (
+          {pinPosition && gameImage && (
             <div className="target">
               <Pin position={pinPosition} setPosition={setPinPosition} />
               <Menu
                 position={{ x: pinPosition.x + 50, y: pinPosition.y - 37 }}
-                items={image.objects}
+                items={gameImage.objects}
                 currentItem={game.objects.current}
                 setFoundObjects={(objectId) => {
                   setPinPosition(null);
@@ -372,7 +366,7 @@ export default function Game() {
                     const gameFoundObjects =
                       game.objects.found.concat(objectId);
                     const gameEnd =
-                      gameFoundObjects.length === image.objects.length;
+                      gameFoundObjects.length === gameImage.objects.length;
                     setGame((prev) => ({
                       ...prev,
                       stop: gameEnd,
